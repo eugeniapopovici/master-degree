b99913dfdd8556687e1cac0b1a32c506f4bdb63a
eugeniapopovici <eugenia.popovici1993@gmail.com>
2016-10-01 14:16:20 +0300

    SQL work zone.

--- src/main/resources/db.changes/db.changelog-1.0.sql
+++ src/main/resources/db.changes/db.changelog-1.0.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset nbalan:1
-CREATE TABLE address (
+CREATE TABLE addresses (
   id integer
     NOT NULL
     PRIMARY KEY,
@@ -17,49 +17,48 @@ CREATE TABLE cinemas (
   cinema_name char(30),
   address_id integer
     NOT NULL
-    REFERENCES address(id),
-  cinema_phone char(20),
-  cinema_seat_capacity integer
+    REFERENCES addresses(id),
+  cinema_phone char(20)
 );
 
 CREATE TABLE movies (
   id integer
     NOT NULL
     PRIMARY KEY,
-  movie_rating integer,
-  movie_name char(30),
+  rating integer,
+  name char(30),
+  description char(30),
   genre char(20),
-  director char(50),
+  stage_director char(50),
   duration integer,
   three_D boolean,
   release_date date
 );
 
---changeset srotari: 2
-CREATE TABLE movie_showings (
+CREATE TABLE performances(
   id integer
     NOT NULL
     PRIMARY KEY,
   cinema_id integer
-    REFERENCES cinemas(id)
-    NOT NULL,
-  movie_id integer
-    REFERENCES movies(id)
-    NOT NULL,
-  showing_from_date date,
-  showing_to_date date
-);
-
-CREATE TABLE row_seats (
-  cinema_id integer
-    NOT NULL
     REFERENCES cinemas(id),
-  row_number integer NOT NULL,
-  seat_count integer,
-
-  CONSTRAINT row_seats_pk
-  PRIMARY KEY (cinema_id, row_number)
+  movie_id integer
+    REFERENCES movies(id),
+  performance_id integer
+    REFERENCES performances(id),
+  performance_start_time time,
+  performance_end_time time
 );
+
+
+
+
+
+
+
+
+
+/*
+
 
 CREATE TABLE customers (
   id integer
@@ -69,26 +68,24 @@ CREATE TABLE customers (
   customer_phone char(20)
 );
 
-CREATE TABLE booking(
+CREATE TABLE categories (
   id integer
     NOT NULL
     PRIMARY KEY,
-  customer_id integer
-    NOT NULL
-    REFERENCES customers(id),
-  booking_for_date date,
-  booking_made_date date NOT NULL,
-  booking_seat_count integer NOT NULL
+  category_name char(20)
 );
 
-CREATE TABLE seat_status(
-  id integer
-    NOT NULL
-    PRIMARY KEY,
-  seat_status boolean
+CREATE TABLE cinema_category (
+  cinema_id integer NOT NULL
+    REFERENCES cinemas(id) ON DELETE CASCADE ,
+  category_id integer NOT NULL
+    REFERENCES categories(id),
+  capacity integer,
+
+  CONSTRAINT cinema_category_pk PRIMARY KEY (cinema_id, category_id)
 );
 
-CREATE TABLE performance_numbers(
+CREATE TABLE performances(
   id integer
     NOT NULL
     PRIMARY KEY,
@@ -96,48 +93,33 @@ CREATE TABLE performance_numbers(
   performance_end_time time
 );
 
---changeset epopovici: 2
-CREATE TABLE all_performance_seats (
+CREATE TABLE cinema_movies(
   cinema_id integer
-    NOT NULL,
-  row_number integer
-    NOT NULL,
-  seat_number integer
-    NOT NULL,
-  performance_date date
-    NOT NULL,
-  performance_number integer
-    REFERENCES performance_numbers(id),
-  seat_status_code integer
-    REFERENCES seat_status(id)
-    NOT NULL,
-  booking_id integer
-    REFERENCES booking(id)
-    NOT NULL,
+    REFERENCES cinemas(id),
+  movie_id integer
+    REFERENCES movies(id),
+  performance_id integer
+    REFERENCES performances(id),
 
-  FOREIGN KEY (cinema_id, row_number)
-  REFERENCES row_seats (cinema_id, row_number),
-  CONSTRAINT all_performance_seats_pk
-  PRIMARY KEY (cinema_id, row_number, seat_number, performance_date)
+  CONSTRAINT cinema_movies_pk PRIMARY KEY (cinema_id, movie_id, performance_id)
 );
 
-CREATE TABLE booked_performance_seats (
-  cinema_id integer
+CREATE TABLE booking(
+  id integer
+    NOT NULL
+    PRIMARY KEY,
+  customer_id integer
     NOT NULL,
-  row_number integer
-    NOT NULL,
-  seat_number integer
-    NOT NULL,
-  performance_date date
-    NOT NULL,
-  performance_number integer
-    REFERENCES performance_numbers(id),
-  booking_id integer
-    REFERENCES booking(id)
-    NOT NULL,
+  cinema_id integer,
+  movie_id integer,
+  performance_id integer,
+  booking_for_date date,
+  booking_made_date date NOT NULL,
+  category_id integer NOT NULL
+    REFERENCES categories(id),
 
-  FOREIGN KEY (cinema_id, row_number)
-  REFERENCES row_seats (cinema_id, row_number),
-  CONSTRAINT booked_performance_seats_pk
-  PRIMARY KEY (cinema_id, row_number, seat_number, performance_date, performance_number)
-);
+  FOREIGN KEY (customer_id)
+    REFERENCES customers(id),
+  FOREIGN KEY (cinema_id, movie_id, performance_id)
+    REFERENCES cinema_movies (cinema_id, movie_id, performance_id)
+);*/
