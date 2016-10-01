cc19f6bfe51f3bc29384194180606cb31cdf28db
Natalia Balan <natashullea@gmail.com>
2016-10-01 09:14:29 +0300

    Add the following tables: address, cinemas and movies.

--- src/main/resources/db.changes/db.changelog-1.0.sql
+++ src/main/resources/db.changes/db.changelog-1.0.sql
@@ -1,7 +1,6 @@
 --liquibase formatted sql
 
 --changeset nbalan:1
-
 CREATE TABLE address (
   id integer
     NOT NULL
@@ -9,4 +8,26 @@ CREATE TABLE address (
   country char(30),
   city char(30),
   address char(50)
+);
+
+CREATE TABLE cinemas (
+  id integer
+    NOT NULL
+    PRIMARY KEY,
+  cinema_name char(30),
+  address_id integer
+    NOT NULL
+    REFERENCES address(id),
+  cinema_phone char(20),
+  cinema_seat_capacity integer
+);
+
+CREATE TABLE movies (
+  id integer
+    NOT NULL
+    PRIMARY KEY,
+  movie_rating integer,
+  movie_name char(30),
+  genre char(20),
+  duration integer
 );
