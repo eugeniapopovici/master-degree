340355a1f403e163af32e1189386d5d49ea0aa59
eugeniapopovici <eugenia.popovici1993@gmail.com>
2016-10-02 15:41:24 +0300

    SQL 02.10.2016.

--- src/main/resources/db.changes/db.changelog-1.0.sql
+++ src/main/resources/db.changes/db.changelog-1.0.sql
@@ -2,124 +2,112 @@
 
 --changeset nbalan:1
 CREATE TABLE addresses (
-  id integer
+  id INTEGER
     NOT NULL
     PRIMARY KEY,
-  country char(30),
-  city char(30),
-  address char(50)
+  country CHAR(30),
+  city CHAR(30),
+  address CHAR(50)
 );
 
 CREATE TABLE cinemas (
-  id integer
+  id INTEGER
     NOT NULL
     PRIMARY KEY,
-  cinema_name char(30),
-  address_id integer
+  cinema_name CHAR(30),
+  address_id INTEGER
     NOT NULL
     REFERENCES addresses(id),
-  cinema_phone char(20)
-);
-
-CREATE TABLE movies (
-  id integer
-    NOT NULL
-    PRIMARY KEY,
-  rating integer,
-  name char(30),
-  description char(30),
-  genre char(20),
-  stage_director char(50),
-  duration integer,
-  three_D boolean,
-  release_date date
-);
-
-CREATE TABLE performances(
-  id integer
-    NOT NULL
-    PRIMARY KEY,
-  cinema_id integer
-    REFERENCES cinemas(id),
-  movie_id integer
-    REFERENCES movies(id),
-  performance_id integer
-    REFERENCES performances(id),
-  performance_start_time time,
-  performance_end_time time
-);
-
-
-
-
-
-
-
-
-
-/*
-
-
-CREATE TABLE customers (
-  id integer
-    NOT NULL
-    PRIMARY KEY,
-  customer_name char(50),
-  customer_phone char(20)
+  cinema_phone CHAR(20)
 );
 
 CREATE TABLE categories (
-  id integer
+  id INTEGER
     NOT NULL
     PRIMARY KEY,
-  category_name char(20)
+  category_name CHAR(1)
 );
 
 CREATE TABLE cinema_category (
-  cinema_id integer NOT NULL
-    REFERENCES cinemas(id) ON DELETE CASCADE ,
-  category_id integer NOT NULL
-    REFERENCES categories(id),
-  capacity integer,
+  cinema_id INTEGER
+    NOT NULL
+    REFERENCES cinemas(id)
+    ON DELETE CASCADE,
+  category_id INTEGER
+    NOT NULL
+    REFERENCES categories(id)
+    ON DELETE CASCADE,
 
-  CONSTRAINT cinema_category_pk PRIMARY KEY (cinema_id, category_id)
+  CONSTRAINT cinema_category_pk
+    PRIMARY KEY (cinema_id, category_id)
+);
+
+--changeset srotari:2
+
+CREATE TABLE movies (
+  id INTEGER
+    NOT NULL
+    PRIMARY KEY,
+  rating INTEGER,
+  name CHAR(30),
+  description CHAR(30),
+  genre CHAR(20),
+  stage_director CHAR(50),
+  duration INTEGER,
+  three_D BOOLEAN,
+  release_date DATE
 );
 
 CREATE TABLE performances(
-  id integer
+  id INTEGER
     NOT NULL
     PRIMARY KEY,
-  performance_start_time time,
-  performance_end_time time
+  performance_start_time TIME,
+  performance_end_time TIME
 );
 
-CREATE TABLE cinema_movies(
-  cinema_id integer
-    REFERENCES cinemas(id),
-  movie_id integer
-    REFERENCES movies(id),
-  performance_id integer
-    REFERENCES performances(id),
+CREATE TABLE showings (
+  id INTEGER
+    NOT NULL
+    PRIMARY KEY,
+  cinema_id INTEGER
+    NOT NULL
+    REFERENCES cinemas(id)
+    ON DELETE CASCADE,
+  movie_id INTEGER
+    NOT NULL
+    REFERENCES movies(id)
+    ON DELETE CASCADE,
+  performance_id INTEGER
+    NOT NULL
+    REFERENCES performances(id)
+    ON DELETE CASCADE
+);
 
-  CONSTRAINT cinema_movies_pk PRIMARY KEY (cinema_id, movie_id, performance_id)
+CREATE TABLE customers (
+  id INTEGER
+    NOT NULL
+    PRIMARY KEY,
+  customer_name CHAR(50),
+  customer_phone CHAR(20)
 );
 
 CREATE TABLE booking(
-  id integer
+  id INTEGER
     NOT NULL
     PRIMARY KEY,
-  customer_id integer
-    NOT NULL,
-  cinema_id integer,
-  movie_id integer,
-  performance_id integer,
-  booking_for_date date,
-  booking_made_date date NOT NULL,
-  category_id integer NOT NULL
-    REFERENCES categories(id),
-
-  FOREIGN KEY (customer_id)
-    REFERENCES customers(id),
-  FOREIGN KEY (cinema_id, movie_id, performance_id)
-    REFERENCES cinema_movies (cinema_id, movie_id, performance_id)
-);*/
+  customer_id INTEGER
+    NOT NULL
+    REFERENCES customers(id)
+    ON DELETE CASCADE,
+  showing_id INTEGER
+    NOT NULL
+    REFERENCES showings(id)
+    ON DELETE CASCADE,
+  category_id INTEGER
+    NOT NULL
+    REFERENCES categories(id)
+    ON DELETE CASCADE ,
+  booking_for_date DATE,
+  booking_made_date DATE NOT NULL
+);
